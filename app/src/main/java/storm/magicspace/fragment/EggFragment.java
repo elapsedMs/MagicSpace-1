b14dcc387d1456aab9ef2d6d70b34b80716123bd
py <992194570@qq.com>
2016-06-16 18:38:06 +0800

    view of egg

--- app/src/main/java/storm/magicspace/fragment/EggFragment.java
+++ app/src/main/java/storm/magicspace/fragment/EggFragment.java
@@ -1,20 +1,36 @@
 package storm.magicspace.fragment;
 
 import android.os.Bundle;
-import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ListView;
 
 import storm.commonlib.common.base.BaseFragment;
+import storm.magicspace.R;
+import storm.magicspace.adapter.EggAdapter;
 
 /**
  * Created by gdq on 16/6/15.
  */
 public class EggFragment extends BaseFragment {
+    private ListView lv_egg;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return super.onCreateView(inflater, container, savedInstanceState);
+        return inflater.inflate(R.layout.fragment_egg, null);
     }
 
+    @Override
+    public void initView(View view) {
+        super.initView(view);
+        lv_egg = this.findItemEventView(view, R.id.lv_egg);
+        EggAdapter adapter = new EggAdapter(getActivity());
+        lv_egg.setAdapter(adapter);
+    }
+
+    @Override
+    public void onLocalItemClicked(AdapterView<?> parent, View view, int position, long id) {
+        super.onLocalItemClicked(parent, view, position, id);
+    }
 }
