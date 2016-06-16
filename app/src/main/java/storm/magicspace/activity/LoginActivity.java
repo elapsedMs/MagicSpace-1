302978af26fd0b1da9b9198c7a49b4bf3fedbcbf
py <992194570@qq.com>
2016-06-16 09:46:38 +0800

    go to mainactivity

--- app/src/main/java/storm/magicspace/activity/LoginActivity.java
+++ app/src/main/java/storm/magicspace/activity/LoginActivity.java
@@ -1,11 +1,13 @@
 package storm.magicspace.activity;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 import storm.commonlib.common.base.BaseActivity;
 import storm.magicspace.R;
 
 public class LoginActivity extends BaseActivity {
+    private Button bt_login;
 
     public LoginActivity() {
         super(R.layout.activity_login);
@@ -14,5 +16,19 @@ public class LoginActivity extends BaseActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        bt_login = findEventView(R.id.bt_login);
+    }
+
+    @Override
+    public void onLocalClicked(int resId) {
+        super.onLocalClicked(resId);
+        switch (resId){
+            case R.id.bt_login:
+            goToNext(MainActivity.class);
+                break;
+
+            default:
+                break;
+        }
     }
 }
