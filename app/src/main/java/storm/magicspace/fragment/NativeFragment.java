229a769a8ffde6791ffa9cfde1e6d118c81fc0cc
关殿清 <gdqgdqgdq@medtree.cn>
2016-06-19 18:44:16 +0800

    album

--- app/src/main/java/storm/magicspace/fragment/NativeFragment.java
+++ app/src/main/java/storm/magicspace/fragment/NativeFragment.java
@@ -5,7 +5,9 @@ import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
 import android.widget.ListView;
+import android.widget.RelativeLayout;
 
 import storm.commonlib.common.base.BaseFragment;
 import storm.magicspace.R;
@@ -19,7 +21,8 @@ public class NativeFragment extends BaseFragment implements View.OnClickListener
 
     private AlbumTitleView albumTitleView;
     private ListView cachedListView;
-
+    private LinearLayout noDownloadLl;
+    private RelativeLayout contentRl;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_native, null);
@@ -33,6 +36,8 @@ public class NativeFragment extends BaseFragment implements View.OnClickListener
         albumTitleView.setOnClickListener(this);
         albumTitleView.showDot();
         cachedListView = (ListView) view.findViewById(R.id.cachedLv);
+        noDownloadLl = (LinearLayout) view.findViewById(R.id.no_download_ll);
+        contentRl = (RelativeLayout) view.findViewById(R.id.rl_content);
     }
 
     @Override
@@ -43,5 +48,15 @@ public class NativeFragment extends BaseFragment implements View.OnClickListener
                 goToNext(CacheingActivity.class);
                 break;
         }
+    }
+
+    public void showNoDownload(){
+        noDownloadLl.setVisibility(View.VISIBLE);
+        contentRl.setVisibility(View.GONE);
+    }
+
+    public void showContent(){
+        noDownloadLl.setVisibility(View.GONE);
+        contentRl.setVisibility(View.VISIBLE);
     }
 }
--- app/src/main/java/storm/magicspace/fragment/OnlineFragment.java
+++ app/src/main/java/storm/magicspace/fragment/OnlineFragment.java
@@ -5,6 +5,7 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
 
 import storm.commonlib.common.base.BaseFragment;
 import storm.magicspace.R;
@@ -16,6 +17,9 @@ import static storm.magicspace.R.layout.*;
  * Created by gdq on 16/6/16.
  */
 public class OnlineFragment extends BaseFragment {
+
+    private LinearLayout noNetWorkLl;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         Log.d("gdq", "OnlineFragment onCreateView");
@@ -23,28 +27,18 @@ public class OnlineFragment extends BaseFragment {
     }
 
     @Override
-    public void onStart() {
-        Log.d("gdq", "OnlineFragment onStart");
-        super.onStart();
-    }
-
-    @Override
-    public void onResume() {
-        Log.d("gdq", "OnlineFragment onResume");
-        super.onResume();
+    public void initView(View view) {
+        super.initView(view);
+        noNetWorkLl = (LinearLayout) view.findViewById(id.no_net_work_ll);
     }
 
 
-    @Override
-    public void onPause() {
-        Log.d("gdq", "OnlineFragment onPause");
-        super.onPause();
+    public void showNoNetWork() {
+        noNetWorkLl.setVisibility(View.VISIBLE);
     }
 
-    @Override
-    public void onStop() {
-        Log.d("gdq", "OnlineFragment onStop");
-        super.onStop();
+    public void showContent() {
+        noNetWorkLl.setVisibility(View.GONE);
     }
 
     @Override
