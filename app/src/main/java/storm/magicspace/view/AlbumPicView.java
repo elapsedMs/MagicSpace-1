18c701c33350a988582c15717d9db59fc9710c92
关殿清 <gdqgdqgdq@medtree.cn>
2016-06-20 16:58:16 +0800

    album

--- app/src/main/java/storm/magicspace/view/AlbumPicView.java
+++ app/src/main/java/storm/magicspace/view/AlbumPicView.java
@@ -2,22 +2,29 @@ package storm.magicspace.view;
 
 import android.content.Context;
 import android.util.AttributeSet;
+import android.view.MotionEvent;
 
 import storm.commonlib.common.base.BaseView;
+import storm.magicspace.R;
 
 /**
  * Created by gdq on 16/6/19.
  */
 public class AlbumPicView extends BaseView {
-    public AlbumPicView(Context context, int viewId) {
-        super(context, viewId);
+    public AlbumPicView(Context context) {
+        this(context, null);
     }
 
-    public AlbumPicView(Context context, AttributeSet attrs, int viewId) {
-        super(context, attrs, viewId);
+    public AlbumPicView(Context context, AttributeSet attrs) {
+        this(context, attrs, -1);
     }
 
-    public AlbumPicView(Context context, AttributeSet attrs, int defStyle, int viewId) {
-        super(context, attrs, defStyle, viewId);
+    public AlbumPicView(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle, R.layout.view_pic);
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        return super.onTouchEvent(event);
     }
 }
