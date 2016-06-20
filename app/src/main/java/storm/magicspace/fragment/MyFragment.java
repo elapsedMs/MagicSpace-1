a01a92be4638b2e1928be520ff92f4260d30f7a2
lixiaolu <erevus@yeah.net>
2016-06-20 15:36:45 +0800

    mine view

--- app/src/main/java/storm/magicspace/fragment/MyFragment.java
+++ app/src/main/java/storm/magicspace/fragment/MyFragment.java
@@ -1,15 +1,25 @@
 package storm.magicspace.fragment;
 
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import storm.commonlib.common.base.BaseFragment;
+import storm.magicspace.R;
 
-/**
- * Created by gdq on 16/6/15.
- */
 public class MyFragment extends BaseFragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_mine, null);
+    }
+
+    @Override
+    public void initView(View view) {
+        super.initView(view);
+    }
 
 }
