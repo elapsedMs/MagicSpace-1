ae87831dd6a764e0da0fe4c3ebe915977ada7a41
lixiaolu <erevus@yeah.net>
2016-06-22 16:11:05 +0800

    add url consttns

---
+++ app/src/main/java/storm/magicspace/http/URLConstant.java
@@ -0,0 +1,15 @@
+package storm.magicspace.http;
+
+/**
+ * Created by lixiaolu on 16/6/22.
+ */
+public class URLConstant {
+
+
+    //    http://app.stemmind.com/3d/api/tp/index.php/getMaterialList
+    private static final String SERVER_DOMAIN_URL = "http://app.stemmind.com/";
+    private static final String SERVER_SECOND_URL = "3d/api/tp/index.php/";
+
+    //获取主页数据
+    public static final String URL_GET_MASTERIAL_LIST = SERVER_DOMAIN_URL + SERVER_SECOND_URL + "getMaterialList";
+}
