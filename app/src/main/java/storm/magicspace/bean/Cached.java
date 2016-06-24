a1b5c36ff0c9d8b61f753a307fbac80859ad4d34
关殿清 <gdqgdqgdq@medtree.cn>
2016-06-24 15:11:32 +0800

    album

--- app/src/main/java/storm/magicspace/bean/Cached.java
+++ app/src/main/java/storm/magicspace/bean/Cached.java
@@ -1,7 +1,199 @@
 package storm.magicspace.bean;
 
+import java.util.List;
+
 /**
  * Created by gdq on 16/6/19.
  */
 public class Cached {
+
+    /**
+     * errCode : 1000
+     * errInfo : success
+     * action :
+     * data : [{"userId":"135601920027846561","nickName":"J-lin","portraitImage":"http://bbs.mojing.cn/uc_server/images/noavatar_big.gif","contentId":"3399","createTime":"1459922401451","duration":"0","url":"http://share.moyan.mojing.cn/MEdata/file/135601920027846561/1459922399.jpg","commentCount":"0","appreciateCount":"0","thumbImageUrl":"http://share.moyan.mojing.cn/MEdata/file/135601920027846561/1459922399_thumb.jpg","contentType":"2","isCollected":"0","isAppreciated":"0","description":"e6bf80e9809f"},{"userId":"135601920019200917","nickName":"狠狠的小老虎","portraitImage":"http://bbs.mojing.cn/uc_server/images/noavatar_big.gif","contentId":"3396","createTime":"1459768966928","duration":"0","url":"http://share.moyan.mojing.cn/MEdata/file/135601920019200917/1459768967362.jpg","commentCount":"0","appreciateCount":"2","thumbImageUrl":"http://share.moyan.mojing.cn/MEdata/file/135601920019200917/1459768967362_thumb.jpg","contentType":"2","isCollected":"0","isAppreciated":"2","description":"留个纪念"},{"userId":"135601920019200917","nickName":"狠狠的小老虎","portraitImage":"http://bbs.mojing.cn/uc_server/images/noavatar_big.gif","contentId":"3394","createTime":"1459767466201","duration":"0","url":"http://share.moyan.mojing.cn/MEdata/file/135601920019200917/1459767466489.jpg","commentCount":"1","appreciateCount":"1","thumbImageUrl":"http://share.moyan.mojing.cn/MEdata/file/135601920019200917/1459767466489_thumb.jpg","contentType":"2","isCollected":"0","isAppreciated":"1","description":"开播"},{"userId":"135601920060277542","nickName":"托总","portraitImage":"http://bbs.mojing.cn/uc_server/images/noavatar_big.gif","contentId":"3388","createTime":"1459048753447","duration":"0","url":"http://share.moyan.mojing.cn/MEdata/file/135601920060277542/1459048752.jpg","commentCount":"1","appreciateCount":"0","thumbImageUrl":"http://share.moyan.mojing.cn/MEdata/file/135601920060277542/1459048752_thumb.jpg","contentType":"2","isCollected":"0","isAppreciated":"0","description":"e58c97e4baace6988ee59f8ee5a299e98197e59d80e585ace59bade79a84e88ab1e584bfe5bc80e4ba86efbc81"},{"userId":"3938569085559701","nickName":"马克西姆","portraitImage":"http://bbs.mojing.cn/uc_server/images/noavatar_big.gif","contentId":"3384","createTime":"1458441052727","duration":"0","url":"http://share.moyan.mojing.cn/MEdata/file/3938569085559701/1458441050.jpg","commentCount":"1","appreciateCount":"0","thumbImageUrl":"http://share.moyan.mojing.cn/MEdata/file/3938569085559701/1458441050_thumb.jpg","contentType":"2","isCollected":"0","isAppreciated":"0","description":"e9a9ace5858be8a5bfe5a786e5b8bde9a5b0e696b0e8a786e7958cefbc81"},{"userId":"3938569085559701","nickName":"马克西姆","portraitImage":"http://bbs.mojing.cn/uc_server/images/noavatar_big.gif","contentId":"3381","createTime":"1458440012819","duration":"0","url":"http://share.moyan.mojing.cn/MEdata/file/3938569085559701/1458440010.jpg","commentCount":"2","appreciateCount":"2","thumbImageUrl":"http://share.moyan.mojing.cn/MEdata/file/3938569085559701/1458440010_thumb.jpg","contentType":"2","isCollected":"0","isAppreciated":"2","description":"e7bbb4e58da12ee78e8be5b8bde9a5b0e8aebee8aea1e5aea4e5b195e58e85efbc81"},{"userId":"135601920060277542","nickName":"托总","portraitImage":"http://bbs.mojing.cn/uc_server/images/noavatar_big.gif","contentId":"3379","createTime":"1458208536613","duration":"0","url":"http://share.moyan.mojing.cn/MEdata/file/135601920060277542/1458208537.jpg","commentCount":"1","appreciateCount":"0","thumbImageUrl":"http://share.moyan.mojing.cn/MEdata/file/135601920060277542/1458208537_thumb.jpg","contentType":"2","isCollected":"0","isAppreciated":"0","description":"e68891e4bbace79a84e58a9ee585ace5aea4"},{"userId":"135601920084975535","nickName":"巴巴爸爸爸爸","portraitImage":"http://bbs.mojing.cn/uc_server/images/noavatar_big.gif","contentId":"3358","createTime":"1456479329322","duration":"0","url":"http://share.moyan.mojing.cn/MEdata/file/135601920084975535/1456479328.jpg","commentCount":"2","appreciateCount":"1","thumbImageUrl":"http://share.moyan.mojing.cn/MEdata/file/135601920084975535/1456479328_thumb.jpg","contentType":"2","isCollected":"0","isAppreciated":"1","description":"e6b1bde8bda6e4b98be5aeb6"},{"userId":"3931156127166035","nickName":"伍福先生","portraitImage":"http://static.mall.mojing.cn/center/ori/f70207805254f98a.jpeg","contentId":"3355","createTime":"1456022516787","duration":"0","url":"http://share.moyan.mojing.cn/MEdata/file/3931156127166035/1456022512859.jpg","commentCount":"1","appreciateCount":"1","thumbImageUrl":"http://share.moyan.mojing.cn/MEdata/file/3931156127166035/1456022512859_thumb.jpg","contentType":"2","isCollected":"0","isAppreciated":"1","description":"杭州西湖 九溪烟树 八宝亭"},{"userId":"3931156127166035","nickName":"伍福先生","portraitImage":"http://static.mall.mojing.cn/center/ori/f70207805254f98a.jpeg","contentId":"3354","createTime":"1456020016915","duration":"0","url":"http://share.moyan.mojing.cn/MEdata/file/3931156127166035/1456020012457.jpg","commentCount":"1","appreciateCount":"2","thumbImageUrl":"http://share.moyan.mojing.cn/MEdata/file/3931156127166035/1456020012457_thumb.jpg","contentType":"2","isCollected":"0","isAppreciated":"2","description":"杭州西湖?九溪十八涧"},{"userId":"3931156127166035","nickName":"伍福先生","portraitImage":"http://static.mall.mojing.cn/center/ori/f70207805254f98a.jpeg","contentId":"3346","createTime":"1455705958724","duration":"0","url":"http://share.moyan.mojing.cn/MEdata/file/3931156127166035/1455705959165.jpg","commentCount":"2","appreciateCount":"1","thumbImageUrl":"http://share.moyan.mojing.cn/MEdata/file/3931156127166035/1455705959165_thumb.jpg","contentType":"2","isCollected":"0","isAppreciated":"1","description":"再次测试"},{"userId":"135601920047963608","nickName":"lanbodd","portraitImage":"http://bbs.mojing.cn/uc_server/images/noavatar_big.gif","contentId":"3343","createTime":"1455510604205","duration":"0","url":"http://share.moyan.mojing.cn/MEdata/file/135601920047963608/1455510599629.jpg","commentCount":"2","appreciateCount":"1","thumbImageUrl":"http://share.moyan.mojing.cn/MEdata/file/135601920047963608/1455510599629_thumb.jpg","contentType":"2","isCollected":"0","isAppreciated":"1","description":"极光隧道"},{"userId":"135601920073501097","nickName":"悟空1127","portraitImage":"http://bbs.mojing.cn/uc_server/images/noavatar_big.gif","contentId":"3338","createTime":"1455073113643","duration":"0","url":"http://share.moyan.mojing.cn/MEdata/file/135601920073501097/1455073112418.jpg","commentCount":"1","appreciateCount":"1","thumbImageUrl":"http://share.moyan.mojing.cn/MEdata/file/135601920073501097/1455073112418_thumb.jpg","contentType":"2","isCollected":"0","isAppreciated":"1","description":"大年初一试试魔眼"},{"userId":"3935929874284385","nickName":"Gnid","portraitImage":"http://sso.mojing.cn/assets/web/images/usercenter/noavatar_big.jpg","contentId":"3333","createTime":"1454514284539","duration":"0","url":"http://share.moyan.mojing.cn/MEdata/file/3935929874284385/1454514282204.jpg","commentCount":"1","appreciateCount":"0","thumbImageUrl":"http://share.moyan.mojing.cn/MEdata/file/3935929874284385/1454514282204_thumb.jpg","contentType":"2","isCollected":"0","isAppreciated":"0","description":"室内篮球场"},{"userId":"3935929874284385","nickName":"Gnid","portraitImage":"http://sso.mojing.cn/assets/web/images/usercenter/noavatar_big.jpg","contentId":"3332","createTime":"1454514224755","duration":"0","url":"http://share.moyan.mojing.cn/MEdata/file/3935929874284385/1454514223222.jpg","commentCount":"1","appreciateCount":"0","thumbImageUrl":"http://share.moyan.mojing.cn/MEdata/file/3935929874284385/1454514223222_thumb.jpg","contentType":"2","isCollected":"0","isAppreciated":"0","description":"绿茵展厅"},{"userId":"3904167920023796","nickName":"黑狗君","portraitImage":"http://static.mall.mojing.cn/center/ori/1ef51ece548c4fb8.jpeg","contentId":"3328","createTime":"1454167447137","duration":"0","url":"http://share.moyan.mojing.cn/MEdata/file/3904167920023796/1454167450720.jpg","commentCount":"1","appreciateCount":"1","thumbImageUrl":"http://share.moyan.mojing.cn/MEdata/file/3904167920023796/1454167450720_thumb.jpg","contentType":"2","isCollected":"0","isAppreciated":"1","description":"安答射箭俱乐部"},{"userId":"3904167920023796","nickName":"黑狗君","portraitImage":"http://static.mall.mojing.cn/center/ori/1ef51ece548c4fb8.jpeg","contentId":"3327","createTime":"1454167340602","duration":"0","url":"http://share.moyan.mojing.cn/MEdata/file/3904167920023796/1454167344207.jpg","commentCount":"1","appreciateCount":"1","thumbImageUrl":"http://share.moyan.mojing.cn/MEdata/file/3904167920023796/1454167344207_thumb.jpg","contentType":"2","isCollected":"0","isAppreciated":"1","description":"安答射箭俱乐部"},{"userId":"3904167920023796","nickName":"黑狗君","portraitImage":"http://static.mall.mojing.cn/center/ori/1ef51ece548c4fb8.jpeg","contentId":"3326","createTime":"1454167301275","duration":"0","url":"http://share.moyan.mojing.cn/MEdata/file/3904167920023796/1454167304770.jpg","commentCount":"1","appreciateCount":"1","thumbImageUrl":"http://share.moyan.mojing.cn/MEdata/file/3904167920023796/1454167304770_thumb.jpg","contentType":"2","isCollected":"0","isAppreciated":"1","description":"花卉大棚"},{"userId":"3904167920023796","nickName":"黑狗君","portraitImage":"http://static.mall.mojing.cn/center/ori/1ef51ece548c4fb8.jpeg","contentId":"3325","createTime":"1454167250988","duration":"0","url":"http://share.moyan.mojing.cn/MEdata/file/3904167920023796/1454167254369.jpg","commentCount":"1","appreciateCount":"1","thumbImageUrl":"http://share.moyan.mojing.cn/MEdata/file/3904167920023796/1454167254369_thumb.jpg","contentType":"2","isCollected":"0","isAppreciated":"1","description":"花卉大棚"},{"userId":"3904167920023796","nickName":"黑狗君","portraitImage":"http://static.mall.mojing.cn/center/ori/1ef51ece548c4fb8.jpeg","contentId":"3321","createTime":"1454063573685","duration":"0","url":"http://share.moyan.mojing.cn/MEdata/file/3904167920023796/1454063575158.jpg","commentCount":"1","appreciateCount":"1","thumbImageUrl":"http://share.moyan.mojing.cn/MEdata/file/3904167920023796/1454063575158_thumb.jpg","contentType":"2","isCollected":"0","isAppreciated":"1","description":"夜幕下的小区\u2026\u2026"}]
+     */
+
+    /**
+     * userId : 135601920027846561
+     * nickName : J-lin
+     * portraitImage : http://bbs.mojing.cn/uc_server/images/noavatar_big.gif
+     * contentId : 3399
+     * createTime : 1459922401451
+     * duration : 0
+     * url : http://share.moyan.mojing.cn/MEdata/file/135601920027846561/1459922399.jpg
+     * commentCount : 0
+     * appreciateCount : 0
+     * thumbImageUrl : http://share.moyan.mojing.cn/MEdata/file/135601920027846561/1459922399_thumb.jpg
+     * contentType : 2
+     * isCollected : 0
+     * isAppreciated : 0
+     * description : e6bf80e9809f
+     */
+
+    private String errCode;
+    private List<DataBean> data;
+    private String errInfo;
+    private String action;
+
+    public String getErrCode() {
+        return errCode;
+    }
+
+    public void setErrCode(String errCode) {
+        this.errCode = errCode;
+    }
+
+    public String getErrInfo() {
+        return errInfo;
+    }
+
+    public void setErrInfo(String errInfo) {
+        this.errInfo = errInfo;
+    }
+
+    public String getAction() {
+        return action;
+    }
+
+    public void setAction(String action) {
+        this.action = action;
+    }
+
+    public List<DataBean> getData() {
+        return data;
+    }
+
+    public void setData(List<DataBean> data) {
+        this.data = data;
+    }
+
+    public static class DataBean {
+        private String userId;
+        private String nickName;
+        private String portraitImage;
+        private String contentId;
+        private String createTime;
+        private String duration;
+        private String url;
+        private String commentCount;
+        private String appreciateCount;
+        private String thumbImageUrl;
+        private String contentType;
+        private String isCollected;
+        private String isAppreciated;
+        private String description;
+
+        public String getUserId() {
+            return userId;
+        }
+
+        public void setUserId(String userId) {
+            this.userId = userId;
+        }
+
+        public String getNickName() {
+            return nickName;
+        }
+
+        public void setNickName(String nickName) {
+            this.nickName = nickName;
+        }
+
+        public String getPortraitImage() {
+            return portraitImage;
+        }
+
+        public void setPortraitImage(String portraitImage) {
+            this.portraitImage = portraitImage;
+        }
+
+        public String getContentId() {
+            return contentId;
+        }
+
+        public void setContentId(String contentId) {
+            this.contentId = contentId;
+        }
+
+        public String getCreateTime() {
+            return createTime;
+        }
+
+        public void setCreateTime(String createTime) {
+            this.createTime = createTime;
+        }
+
+        public String getDuration() {
+            return duration;
+        }
+
+        public void setDuration(String duration) {
+            this.duration = duration;
+        }
+
+        public String getUrl() {
+            return url;
+        }
+
+        public void setUrl(String url) {
+            this.url = url;
+        }
+
+        public String getCommentCount() {
+            return commentCount;
+        }
+
+        public void setCommentCount(String commentCount) {
+            this.commentCount = commentCount;
+        }
+
+        public String getAppreciateCount() {
+            return appreciateCount;
+        }
+
+        public void setAppreciateCount(String appreciateCount) {
+            this.appreciateCount = appreciateCount;
+        }
+
+        public String getThumbImageUrl() {
+            return thumbImageUrl;
+        }
+
+        public void setThumbImageUrl(String thumbImageUrl) {
+            this.thumbImageUrl = thumbImageUrl;
+        }
+
+        public String getContentType() {
+            return contentType;
+        }
+
+        public void setContentType(String contentType) {
+            this.contentType = contentType;
+        }
+
+        public String getIsCollected() {
+            return isCollected;
+        }
+
+        public void setIsCollected(String isCollected) {
+            this.isCollected = isCollected;
+        }
+
+        public String getIsAppreciated() {
+            return isAppreciated;
+        }
+
+        public void setIsAppreciated(String isAppreciated) {
+            this.isAppreciated = isAppreciated;
+        }
+
+        public String getDescription() {
+            return description;
+        }
+
+        public void setDescription(String description) {
+            this.description = description;
+        }
+    }
 }
