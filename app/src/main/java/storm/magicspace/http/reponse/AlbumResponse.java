b39a0387e9b19d239d88996ed51a4d66a557f64e
关殿清 <gdqgdqgdq@medtree.cn>
2016-06-24 15:30:58 +0800

    album

---
+++ app/src/main/java/storm/magicspace/http/reponse/AlbumResponse.java
@@ -0,0 +1,9 @@
+package storm.magicspace.http.reponse;
+
+import storm.commonlib.common.http.baseHttpBean.BaseResponse;
+
+/**
+ * Created by gdq on 16/6/24.
+ */
+public class AlbumResponse extends BaseResponse {
+}
