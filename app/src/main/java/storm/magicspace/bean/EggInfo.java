76840ae8c29ceb9d30917dcce600e3f69c5e1490
pengyong <992194570@qq.com>
2016-06-26 10:10:41 +0800

    get data for egg

---
+++ app/src/main/java/storm/magicspace/bean/EggInfo.java
@@ -0,0 +1,21 @@
+package storm.magicspace.bean;
+
+/**
+ * Created by pengyong on 2016/6/25.
+ */
+public class EggInfo {
+    public String userId;
+    public String nickName;
+    public String portraitImage;
+    public String contentId;
+    public String createTime;
+    public String duration;
+    public String url;
+    public String commentCount;
+    public String appreciateCount;
+    public String thumbImageUrl;
+    public String contentType;
+    public String isCollected;
+    public String isAppreciated;
+    public String description;
+}
