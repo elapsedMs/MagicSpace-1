b8d5e0865c8d73706b233784f7739d59295356ae
pengyong <992194570@qq.com>
2016-06-27 22:55:02 +0800

    add share sdk

--- app/src/main/java/storm/magicspace/activity/AboutUsActivity.java
+++ app/src/main/java/storm/magicspace/activity/AboutUsActivity.java
@@ -7,6 +7,8 @@ import android.widget.ImageView;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 
+import cn.sharesdk.framework.ShareSDK;
+import cn.sharesdk.onekeyshare.OnekeyShare;
 import storm.commonlib.common.CommonConstants;
 import storm.commonlib.common.base.BaseActivity;
 import storm.magicspace.R;
@@ -44,7 +46,7 @@ public class AboutUsActivity extends BaseActivity {
         super.onLocalClicked(resId);
         switch (resId) {
             case R.id.rl_share:
-
+                showShare();
                 break;
 
             case R.id.rl_assessment:
@@ -54,5 +56,34 @@ public class AboutUsActivity extends BaseActivity {
             default:
                 break;
         }
+    }
+
+    private void showShare() {
+        ShareSDK.initSDK(AboutUsActivity.this);
+        OnekeyShare oks = new OnekeyShare();
+        //关闭sso授权
+        oks.disableSSOWhenAuthorize();
+
+// 分享时Notification的图标和文字  2.5.9以后的版本不调用此方法
+        //oks.setNotification(R.drawable.ic_launcher, getString(R.string.app_name));
+        // title标题，印象笔记、邮箱、信息、微信、人人网和QQ空间使用
+        oks.setTitle(getString(R.string.share));
+        // titleUrl是标题的网络链接，仅在人人网和QQ空间使用
+        oks.setTitleUrl("http://app.stemmind.com/vr/a/tour.html");
+        // text是分享文本，所有平台都需要这个字段
+        oks.setText("我是分享文本");
+        // imagePath是图片的本地路径，Linked-In以外的平台都支持此参数
+        //oks.setImagePath("/sdcard/test.jpg");//确保SDcard下面存在此张图片
+        // url仅在微信（包括好友和朋友圈）中使用
+//        oks.setUrl("http://app.stemmind.com/vr/a/tour.html");
+        // comment是我对这条分享的评论，仅在人人网和QQ空间使用
+        oks.setComment("我是测试评论文本");
+        // site是分享此内容的网站名称，仅在QQ空间使用
+        oks.setSite(getString(R.string.app_name));
+        // siteUrl是分享此内容的网站地址，仅在QQ空间使用
+        oks.setSiteUrl("http://app.stemmind.com/vr/a/tour.html");
+
+// 启动分享GUI
+        oks.show(AboutUsActivity.this);
     }
 }
--- app/src/main/java/storm/magicspace/activity/EggGamePreviewActivity.java
+++ app/src/main/java/storm/magicspace/activity/EggGamePreviewActivity.java
@@ -2,10 +2,12 @@ package storm.magicspace.activity;
 
 import android.annotation.SuppressLint;
 import android.app.Activity;
+import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.OrientationHelper;
 import android.support.v7.widget.RecyclerView;
+import android.text.TextUtils;
 import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
@@ -17,17 +19,28 @@ import android.widget.ImageView;
 import android.widget.RelativeLayout;
 import android.widget.SeekBar;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import java.util.List;
+
+import cn.sharesdk.framework.ShareSDK;
+import cn.sharesdk.onekeyshare.OnekeyShare;
+import storm.commonlib.common.base.BaseASyncTask;
 import storm.commonlib.common.util.LogUtil;
 import storm.magicspace.R;
+import storm.magicspace.adapter.EggsAdapter;
+import storm.magicspace.bean.httpBean.EggImage;
+import storm.magicspace.bean.httpBean.EggImageListResponse;
+import storm.magicspace.http.HTTPManager;
 import storm.magicspace.view.FloatView;
 import storm.magicspace.view.FloatView.FloatInfo;
 
 public class EggGamePreviewActivity extends Activity {
 
-    public static final String TAG = EggGamePreviewActivity.class.getSimpleName();
+    public static final String TAG = GameActivity.class.getSimpleName();
     public static final String ALPHA_CONTROLLER_POSITION_PARENT_BOTTOM = "bottom";
     public static final String ALPHA_CONTROLLER_POSITION_ABOVE_EGGS = "above_eggs";
+    public static final int EGG_INIT_COUNT = 5;
 
     private WebView mWebView;
     private FloatView mFloatView;
@@ -35,18 +48,21 @@ public class EggGamePreviewActivity extends Activity {
     private FloatInfo mFloatInfo;
     private SeekBar mAlphaController;
     private float mAlphaVal = 1.0f;
-    private int mItemId = 1;
+    private int mEggsCount = 1;
     private ImageView mGuide;
     private RelativeLayout mEggsContainer;
     private RecyclerView mEggsLayout;
     private TextView mShowEggBtn;
     private boolean isAlphaControllerShowing = false;
     private TextView mEggsLoadingHint;
+    private EggsAdapter mEggsAdapter;
+    private ImageView mCreateEggBtn;
+    private String mUrl;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_egg_game_preview);
+        setContentView(R.layout.activity_game);
 
         initView();
         initEvent();
@@ -62,6 +78,7 @@ public class EggGamePreviewActivity extends Activity {
         mEggsContainer = (RelativeLayout) findViewById(R.id.rl_game_eggs_container);
         mEggsLayout = (RecyclerView) findViewById(R.id.rv_game_eggs);
         mEggsLoadingHint = (TextView) findViewById(R.id.tv_game_loading);
+        mCreateEggBtn = (ImageView) findViewById(R.id.iv_game_confirm);
 
         initFloatView();
         initWebView();
@@ -72,15 +89,47 @@ public class EggGamePreviewActivity extends Activity {
 
     private void initEggs() {
         //mEggsLayout.setLayoutManager(new LinearLayoutManager(this, OrientationHelper.HORIZONTAL,false));
+        updateEggsCountHint(EGG_INIT_COUNT);
         mEggsLayout.setLayoutManager(new GridLayoutManager(this, 1, OrientationHelper.HORIZONTAL, false));
-        //mEggsLayout.setAdapter(new EggsAdapter(null));
+        new GetEggImageListTask().execute();
+
+    }
+
+    private class GetEggImageListTask extends BaseASyncTask<Void, EggImageListResponse> {
+        @Override
+        public EggImageListResponse doRequest(Void param) {
+            return HTTPManager.getEggImageList();
+        }
+
+        @Override
+        protected void onPostExecute(EggImageListResponse eggImageListResponse) {
+            if (eggImageListResponse != null) {
+                mEggsLoadingHint.setVisibility(View.INVISIBLE);
+                mEggsLayout.setVisibility(View.VISIBLE);
+                List<EggImage> data = eggImageListResponse.getData();
+                EggImage eggImage = data.get(0);
+                mEggsAdapter = new EggsAdapter(EggGamePreviewActivity.this, eggImage);
+                mEggsLayout.setAdapter(mEggsAdapter);
+                mEggsAdapter.setOnClickListener(new EggsAdapter.ClickInterface() {
+                    @Override
+                    public void onClick(int position, String url, Bitmap bitmap) {
+                        LogUtil.d(TAG, "position = " + position + ", url = " + url);
+                        mFloatView.setImageBitmap(bitmap);
+                        mFloatInfo = null;
+                        mUrl = url;
+                        initFloatView();
+                    }
+                });
+            } else {
+                mEggsLoadingHint.setText(R.string.loading_failed);
+            }
+        }
     }
 
     private void createEgg() {
         if (mFloatInfo != null) {
             String contentId = "1";
-            int itemId = mItemId++;
-            String url = "http://app.stemmind.com/vr/objs/25.png";
+            int itemId = mEggsCount++;
             float alpha = mAlphaVal;
             float scale = mFloatInfo.getScale();
             float rotate = -mFloatInfo.getRotate();
@@ -91,7 +140,20 @@ public class EggGamePreviewActivity extends Activity {
             mWebView.loadUrl("javascript:dropItem('"
                     + contentId + "' ,'"
                     + itemId + "' ,'"
-                    + url + "' ,'"
+                    + mUrl + "' ,'"
+                    + alpha + "' ,'"
+                    + scale + "' ,'"
+                    + rotate + "')");
+        } else {
+            String contentId = "1";
+            int itemId = mEggsCount++;
+            float alpha = mAlphaVal;
+            float scale = 1.0f;
+            float rotate = 0.0f;
+            mWebView.loadUrl("javascript:dropItem('"
+                    + contentId + "' ,'"
+                    + itemId + "' ,'"
+                    + mUrl + "' ,'"
                     + alpha + "' ,'"
                     + scale + "' ,'"
                     + rotate + "')");
@@ -99,10 +161,10 @@ public class EggGamePreviewActivity extends Activity {
     }
 
     private void initEvent() {
-        mConfirmBtn.setOnClickListener(new View.OnClickListener() {
+        mCreateEggBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                createEgg();
+                showShare();
             }
         });
         mShowEggBtn.setOnClickListener(new View.OnClickListener() {
@@ -112,6 +174,10 @@ public class EggGamePreviewActivity extends Activity {
                 positionAlphaController(ALPHA_CONTROLLER_POSITION_ABOVE_EGGS);
             }
         });
+    }
+
+    private void updateEggsCountHint(int count) {
+        mShowEggBtn.setText(String.format(getResources().getString(R.string.eggs_count), count));
     }
 
     private void initAlphaController() {
@@ -138,7 +204,7 @@ public class EggGamePreviewActivity extends Activity {
     }
 
     private void initFloatView() {
-        mFloatView.setImageResource(R.mipmap.surprise_egg_red);
+        // mFloatView.setImageResource(R.mipmap.surprise_egg_red);
         mFloatView.setOnFloatListener(new FloatView.FloatListener() {
             @Override
             public void clickLeftTop() {
@@ -217,6 +283,35 @@ public class EggGamePreviewActivity extends Activity {
             LogUtil.d(TAG, "receive msg :" + msg);
             // {"x":"0","y":"0","scale":"0.5","alpha":"0.5","rotate":"0"}
         }
+    }
+
+    private void showShare() {
+        ShareSDK.initSDK(EggGamePreviewActivity.this);
+        OnekeyShare oks = new OnekeyShare();
+        //关闭sso授权
+        oks.disableSSOWhenAuthorize();
+
+// 分享时Notification的图标和文字  2.5.9以后的版本不调用此方法
+        //oks.setNotification(R.drawable.ic_launcher, getString(R.string.app_name));
+        // title标题，印象笔记、邮箱、信息、微信、人人网和QQ空间使用
+        oks.setTitle(getString(R.string.share));
+        // titleUrl是标题的网络链接，仅在人人网和QQ空间使用
+        oks.setTitleUrl("http://app.stemmind.com/vr/a/tour.html");
+        // text是分享文本，所有平台都需要这个字段
+        oks.setText("我是分享文本");
+        // imagePath是图片的本地路径，Linked-In以外的平台都支持此参数
+        //oks.setImagePath("/sdcard/test.jpg");//确保SDcard下面存在此张图片
+        // url仅在微信（包括好友和朋友圈）中使用
+//        oks.setUrl("http://app.stemmind.com/vr/a/tour.html");
+        // comment是我对这条分享的评论，仅在人人网和QQ空间使用
+        oks.setComment("我是测试评论文本");
+        // site是分享此内容的网站名称，仅在QQ空间使用
+        oks.setSite(getString(R.string.app_name));
+        // siteUrl是分享此内容的网站地址，仅在QQ空间使用
+        oks.setSiteUrl("http://app.stemmind.com/vr/a/tour.html");
+
+// 启动分享GUI
+        oks.show(EggGamePreviewActivity.this);
     }
 }
 
