987f9dc4812ea22544f7d35a910f5a387880a311
xt <dawn2016@126.com>
2016-06-28 22:22:49 +0800

    add issue request and update request

---
+++ app/src/main/java/storm/magicspace/bean/IssueUCGContent.java
@@ -0,0 +1,7 @@
+package storm.magicspace.bean;
+
+/**
+ * Created by xt on 16/6/28.
+ */
+public class IssueUCGContent {
+}
---
+++ app/src/main/java/storm/magicspace/bean/httpBean/UpdateUGCContentScenesResponse.java
@@ -0,0 +1,9 @@
+package storm.magicspace.bean.httpBean;
+
+import storm.commonlib.common.http.baseHttpBean.BaseResponse;
+
+/**
+ * Created by xt on 16/6/28.
+ */
+public class UpdateUGCContentScenesResponse extends BaseResponse<String>{
+}
