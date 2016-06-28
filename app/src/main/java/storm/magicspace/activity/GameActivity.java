f101a925b55c277ffd79ce0b0fbe1c6bb9f41672
py <992194570@qq.com>
2016-06-28 10:47:16 +0800

    add game share

--- app/src/main/java/storm/magicspace/activity/GameActivity.java
+++ app/src/main/java/storm/magicspace/activity/GameActivity.java
@@ -7,7 +7,6 @@ import android.os.Bundle;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.OrientationHelper;
 import android.support.v7.widget.RecyclerView;
-import android.text.TextUtils;
 import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
@@ -19,10 +18,11 @@ import android.widget.ImageView;
 import android.widget.RelativeLayout;
 import android.widget.SeekBar;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import java.util.List;
 
+import cn.sharesdk.framework.ShareSDK;
+import cn.sharesdk.onekeyshare.OnekeyShare;
 import storm.commonlib.common.base.BaseASyncTask;
 import storm.commonlib.common.util.LogUtil;
 import storm.magicspace.R;
@@ -167,16 +167,18 @@ public class GameActivity extends Activity {
         mCreateEggBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if (TextUtils.isEmpty(mUrl)) {
-                    Toast.makeText(GameActivity.this, R.string.add_egg_hint, Toast.LENGTH_SHORT).show();
-                    return;
-                }
-                if (mEggsCount > EGG_INIT_COUNT) {
-                    Toast.makeText(GameActivity.this, R.string.add_egg_over_hint, Toast.LENGTH_SHORT).show();
-                    return;
-                }
-                updateEggsCountHint(EGG_INIT_COUNT - mEggsCount);
-                createEgg();
+                showShare();
+//                if (TextUtils.isEmpty(mUrl)) {
+//                    Toast.makeText(GameActivity.this, R.string.add_egg_hint, Toast.LENGTH_SHORT).show();
+//                    return;
+//                }
+//                if (mEggsCount > EGG_INIT_COUNT) {
+//                    Toast.makeText(GameActivity.this, R.string.add_egg_over_hint, Toast.LENGTH_SHORT).show();
+//                    return;
+//                }
+//                updateEggsCountHint(EGG_INIT_COUNT - mEggsCount);
+//                createEgg();
+
             }
         });
         mShowEggBtn.setOnClickListener(new View.OnClickListener() {
@@ -295,6 +297,35 @@ public class GameActivity extends Activity {
             LogUtil.d(TAG, "receive msg :" + msg);
             // {"x":"0","y":"0","scale":"0.5","alpha":"0.5","rotate":"0"}
         }
+    }
+
+    private void showShare() {
+        ShareSDK.initSDK(GameActivity.this);
+        OnekeyShare oks = new OnekeyShare();
+        //关闭sso授权
+        oks.disableSSOWhenAuthorize();
+
+// 分享时Notification的图标和文字  2.5.9以后的版本不调用此方法
+        //oks.setNotification(R.drawable.ic_launcher, getString(R.string.app_name));
+        // title标题，印象笔记、邮箱、信息、微信、人人网和QQ空间使用
+        oks.setTitle(getString(R.string.share));
+        // titleUrl是标题的网络链接，仅在人人网和QQ空间使用
+        oks.setTitleUrl("http://app.stemmind.com/vr/a/tour.html");
+        // text是分享文本，所有平台都需要这个字段
+        oks.setText("我是分享文本");
+        // imagePath是图片的本地路径，Linked-In以外的平台都支持此参数
+        //oks.setImagePath("/sdcard/test.jpg");//确保SDcard下面存在此张图片
+        // url仅在微信（包括好友和朋友圈）中使用
+//        oks.setUrl("http://app.stemmind.com/vr/a/tour.html");
+        // comment是我对这条分享的评论，仅在人人网和QQ空间使用
+        oks.setComment("我是测试评论文本");
+        // site是分享此内容的网站名称，仅在QQ空间使用
+        oks.setSite(getString(R.string.app_name));
+        // siteUrl是分享此内容的网站地址，仅在QQ空间使用
+        oks.setSiteUrl("http://app.stemmind.com/vr/a/tour.html");
+
+// 启动分享GUI
+        oks.show(GameActivity.this);
     }
 }
 
