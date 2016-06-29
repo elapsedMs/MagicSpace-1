367ae6fc33831b1af78cd05145b77b3db2e46486
lixiaolu <erevus@yeah.net>
2016-06-30 07:52:12 +0800

    user info

--- app/src/main/java/storm/magicspace/bean/UserInfo.java
+++ app/src/main/java/storm/magicspace/bean/UserInfo.java
@@ -5,4 +5,117 @@ package storm.magicspace.bean;
  */
 public class UserInfo {
 
+    /**
+     * userId : 1
+     * nickname : 官方
+     * portraitImage : http://bbs.mojing.cn/uc_server/images/noavatar_big.gif
+     * mobile : 138*****000
+     * totalLogin : 8
+     * totalAppreciate : 0
+     * totalIssue : 0
+     * totalCollect : 0
+     * totalCredit : 15
+     * totalScore : 0
+     * totalPlay : 0
+     */
+
+    private String userId;
+    private String nickname;
+    private String portraitImage;
+    private String mobile;
+    private String totalLogin;
+    private String totalAppreciate;
+    private String totalIssue;
+    private String totalCollect;
+    private String totalCredit;
+    private String totalScore;
+    private String totalPlay;
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public String getNickname() {
+        return nickname;
+    }
+
+    public void setNickname(String nickname) {
+        this.nickname = nickname;
+    }
+
+    public String getPortraitImage() {
+        return portraitImage;
+    }
+
+    public void setPortraitImage(String portraitImage) {
+        this.portraitImage = portraitImage;
+    }
+
+    public String getMobile() {
+        return mobile;
+    }
+
+    public void setMobile(String mobile) {
+        this.mobile = mobile;
+    }
+
+    public String getTotalLogin() {
+        return totalLogin;
+    }
+
+    public void setTotalLogin(String totalLogin) {
+        this.totalLogin = totalLogin;
+    }
+
+    public String getTotalAppreciate() {
+        return totalAppreciate;
+    }
+
+    public void setTotalAppreciate(String totalAppreciate) {
+        this.totalAppreciate = totalAppreciate;
+    }
+
+    public String getTotalIssue() {
+        return totalIssue;
+    }
+
+    public void setTotalIssue(String totalIssue) {
+        this.totalIssue = totalIssue;
+    }
+
+    public String getTotalCollect() {
+        return totalCollect;
+    }
+
+    public void setTotalCollect(String totalCollect) {
+        this.totalCollect = totalCollect;
+    }
+
+    public String getTotalCredit() {
+        return totalCredit;
+    }
+
+    public void setTotalCredit(String totalCredit) {
+        this.totalCredit = totalCredit;
+    }
+
+    public String getTotalScore() {
+        return totalScore;
+    }
+
+    public void setTotalScore(String totalScore) {
+        this.totalScore = totalScore;
+    }
+
+    public String getTotalPlay() {
+        return totalPlay;
+    }
+
+    public void setTotalPlay(String totalPlay) {
+        this.totalPlay = totalPlay;
+    }
 }
