f70a55fa0183ed5fab491b4ca3fa3a70eb0d55eb
关殿清 <gdqgdqgdq@medtree.cn>
2016-07-05 02:09:41 +0800

    dialog list

--- app/src/main/java/storm/magicspace/activity/MainActivity.java
+++ app/src/main/java/storm/magicspace/activity/MainActivity.java
@@ -7,9 +7,13 @@ import android.support.v4.view.ViewPager;
 import android.util.Log;
 import android.view.KeyEvent;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ImageView;
 
+import org.greenrobot.eventbus.EventBus;
+
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 import storm.commonlib.common.base.BaseASyncTask;
@@ -18,6 +22,8 @@ import storm.commonlib.common.http.baseHttpBean.BaseResponse;
 import storm.commonlib.common.view.TitleBar;
 import storm.magicspace.R;
 import storm.magicspace.adapter.HomeViewPagerAdapter;
+import storm.magicspace.dialog.ListDialog;
+import storm.magicspace.event.ViewPagerEvent;
 import storm.magicspace.fragment.EggFragment;
 import storm.magicspace.fragment.MyFragment;
 import storm.magicspace.fragment.SettingFragment;
@@ -27,6 +33,9 @@ import storm.magicspace.http.reponse.AlbumResponse;
 import storm.magicspace.view.HomeTabView;
 
 public class MainActivity extends BaseActivity implements ViewPager.OnPageChangeListener {
+
+    private final String[] data = new String[]{"从图库选择", "从收藏选择", "随机"};
+
     private static final int ALBUM = 1;
     private static final int EGG = 2;
     private static final int MY = 3;
@@ -40,6 +49,7 @@ public class MainActivity extends BaseActivity implements ViewPager.OnPageChange
     private HomeTabView settingHtv;
     private ImageView addIv;
     private Handler handler;
+    private ListDialog listDialog;
 
     public MainActivity() {
         super(R.layout.activity_main);
@@ -92,6 +102,7 @@ public class MainActivity extends BaseActivity implements ViewPager.OnPageChange
 
     private void initViewPager() {
         AlbumFragment albumFragment = new AlbumFragment();
+        EventBus.getDefault().register(albumFragment);
         EggFragment eggFragment = new EggFragment();
         MyFragment myFragment = new MyFragment();
         SettingFragment settingFragment = new SettingFragment();
@@ -156,7 +167,34 @@ public class MainActivity extends BaseActivity implements ViewPager.OnPageChange
                 viewPager.setCurrentItem(3);
                 break;
             case R.id.add_Btn:
-                goToNext(GameActivity.class);
+                ListDialog.Buider buider = new ListDialog.Buider(MainActivity.this);
+                listDialog = buider.setData(Arrays.asList(data)).setItemClickListener(new AdapterView.OnItemClickListener() {
+                    @Override
+                    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                        switch (position) {
+                            //在线
+                            case 0:
+                                ViewPagerEvent viewPagerEvent = new ViewPagerEvent();
+                                viewPagerEvent.position = 0;
+                                EventBus.getDefault().post(viewPagerEvent);
+                                viewPager.setCurrentItem(0);
+                                break;
+                            //收藏
+                            case 1:
+                                ViewPagerEvent viewPagerEvent1 = new ViewPagerEvent();
+                                viewPagerEvent1.position = 1;
+                                EventBus.getDefault().post(viewPagerEvent1);
+                                viewPager.setCurrentItem(0);
+                                break;
+                            //随机
+                            case 2:
+                                goToNext(GameActivity.class);
+                                break;
+                        }
+                        listDialog.dismiss();
+                    }
+                }).create();
+                listDialog.show();
                 break;
         }
     }
---
+++ app/src/main/java/storm/magicspace/event/ViewPagerEvent.java
@@ -0,0 +1,8 @@
+package storm.magicspace.event;
+
+/**
+ * Created by gdq on 16/7/4.
+ */
+public class ViewPagerEvent {
+    public int position;
+}
