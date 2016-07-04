344a3ebfaa728c4d71165884e4c4eb9db51481a0
lixiaolu <erevus@yeah.net>
2016-07-04 11:29:01 +0800

    png

--- app/src/main/java/storm/magicspace/activity/SplashActivity.java
+++ app/src/main/java/storm/magicspace/activity/SplashActivity.java
@@ -3,6 +3,8 @@ package storm.magicspace.activity;
 import android.os.Bundle;
 
 import storm.commonlib.common.base.BaseActivity;
+import storm.commonlib.common.util.JsonUtil;
+import storm.commonlib.common.util.LogUtil;
 import storm.magicspace.R;
 
 import static storm.commonlib.common.CommonConstants.ACTIVITY_STYLE_EMPTY;
@@ -17,5 +19,9 @@ public class SplashActivity extends BaseActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         goToNext(LoginActivity.class);
+
+        String[] text = new String[0];
+        String s = JsonUtil.convertObjectToJson(text);
+        LogUtil.i("Test", s);
     }
 }
