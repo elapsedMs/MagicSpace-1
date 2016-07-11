d0e83e25ccd7da1c387f02661029a2c7ba0d1f52
py <992194570@qq.com>
2016-07-11 14:18:07 +0800

    fix weibo share

---
+++ app/src/main/java/storm/magicspace/WBShareActivity.java
@@ -0,0 +1,9 @@
+package storm.magicspace;
+
+import com.umeng.socialize.media.WBShareCallBackActivity;
+
+/**
+ * Created by py on 2016/7/11.
+ */
+public class WBShareActivity extends WBShareCallBackActivity {
+}
